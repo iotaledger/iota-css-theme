8418a52a8fe622b482cd1ac6cf70f40022d51557
Rihards Gravis <rihards.gravis@iota.org>
2018-12-18 21:00:58 +0200

    - Update icon usage - Floating menu hover effect - Header layout

--- assets/icons/icons.js
+++ assets/icons/icons.js
@@ -1,9 +1,11 @@
 module.exports = {
    "arrow-right": "→",
+   "burger": "🍔",
    "chevron-up": "⬆",
    "chevron-down": "⬇️",
    "chevron-right": "➡️",
    "chevron-left": "⬅️",
+   "cross": "✖︎",
    "feedback": "💬",
    "search": '🔍',
    "thumb-up": "👍",
